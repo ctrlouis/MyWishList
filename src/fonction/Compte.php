95b932b138de788a7f8b1b07b352a9ffc94cc883
ctrlouis <noem@il.ru>
2019-05-14 01:40:32 +0200

    modification du css pour la page du compte

--- src/fonction/Compte.php
+++ src/fonction/Compte.php
@@ -13,21 +13,41 @@ class Compte {
 		$user = User::where('id', '=', $_SESSION['wishlist_userid'])
 			->first();
 
-		echo '<br>Username : ' . $user->email;
-
+		echo '
+		<div class= "row column align-center medium-6 large-4">
+		<ul class="compte-box">
+			<li class="username">';
+		echo '<strong>' . $user->email . '</strong>';
+		echo '
+			</li>
+			<li>';
 		if ($user->last_name) echo 'Nom : ' . $user->last_name;
 		else echo 'Nom : non renseigné';
-
+		echo '
+			</li>
+			<li>';
 		if ($user->first_name) echo 'Prenom : ' . $user->first_name;
 		else echo 'Prenom : non renseigné';
+		echo '
+			</li>
+		</ul>
+		</div>';
 	}
 
 	public static function compteEditForm() {
 		echo '
 			<form action="edit-compte" method="post">
-				<p><input type="text" name="last_name" placeholder="Last name"/></p>
-				<p><input type="text" name="first_name" placeholder="First name"/></p>
-				<p><input type="submit" name="" value="Modifier"></p>
+
+				<div class= "row align-center medium-5 large-3">
+					<input type="text" name="last_name" placeholder="Nom"/>
+				</div>
+				<div class="row align-center medium-5 large-3">
+					<input type="text" name="first_name" placeholder="Prenom"/>
+				</div>
+				<div class="row align-left medium-5 large-3">
+					<button type="submit" class="button" name="">Modifier</button>
+				</div>
+
 			</form>';
 	}
 
@@ -38,7 +58,8 @@ class Compte {
 		if ($_POST['last_name'] && $_POST['last_name'] != '') $user->last_name = $_POST['last_name'];
 		if ($_POST['first_name'] && $_POST['first_name'] != '') $user->first_name = $_POST['first_name'];
 		$user->save();
-		echo 'Compte modifié';
+		echo 'Compte modifié ! Redirection en cours...';
+		header('Refresh: 0; url=index.php');
 	}
 
 	public static function compteChangePasswordForm() {
